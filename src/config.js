306399b7dd01b9d1ff5528ef358b5dd72933a625
Rajmohan <rajtradinglog@gmail.com>
2026-01-15 12:26:28 +0530

    url changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const IS_PRODUCTION = true; // true = domain, false = localhost
 
 export const APPLICATION_URL = IS_PRODUCTION
-  ? "https://my-trading-log.onrender.com/"
+  ? "https://my-trading-log.onrender.com"
   : "http://localhost:3000";
